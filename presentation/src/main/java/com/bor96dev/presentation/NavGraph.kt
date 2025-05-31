477b56001fdd2e31cc8b0e95db752cfd427cafe0
jakobakoba <borisgabyshev96dev@gmail.com>
2025-05-31 20:44:55 +0900

    create navGraph and destinations

--- presentation/src/main/java/com/bor96dev/presentation/NavGraph.kt
+++ presentation/src/main/java/com/bor96dev/presentation/NavGraph.kt
@@ -2,8 +2,30 @@ package com.bor96dev.presentation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.bor96dev.presentation.navigation.Destinations
 
 @Composable
 fun NavGraph(navController: NavHostController){
+    NavHost(
+        navController = navController,
+        startDestination = Destinations.Search.route
+    ) {
+        composable(Destinations. Search.route){
 
+        }
+        composable(Destinations.Favorites.route){
+
+        }
+        composable(Destinations.Responses.route){
+
+        }
+        composable(Destinations.Messages.route){
+
+        }
+        composable(Destinations.Profile.route){
+
+        }
+    }
 }
