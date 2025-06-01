d774f9720f057be6fd8673e90ae6be0043f73d2d
jakobakoba <borisgabyshev96dev@gmail.com>
2025-06-01 19:29:16 +0900

    favorite screen ui

--- presentation/src/main/java/com/bor96dev/presentation/composables/BottomNavigationBar.kt
+++ presentation/src/main/java/com/bor96dev/presentation/composables/BottomNavigationBar.kt
@@ -1,20 +1,36 @@
 package com.bor96dev.presentation.composables
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.bor96dev.presentation.MainViewModel
 import com.bor96dev.presentation.R
 import com.bor96dev.presentation.navigation.Destinations
+import com.bor96dev.presentation.ui.theme.White
 
 @Composable
-fun BottomNavigationBar(navController: NavController) {
+fun BottomNavigationBar(
+    navController: NavController,
+    viewModel: MainViewModel = hiltViewModel()
+    ) {
+
+    val uiState by viewModel.uiState.collectAsState()
+    val favoriteCount = uiState.vacancies.count{it.isFavorite}
 
     val items = listOf(
         Destinations.Search,
@@ -37,10 +53,31 @@ fun BottomNavigationBar(navController: NavController) {
                     }
                 },
                 icon = {
-                    Icon(
-                        painter = painterResource(id = findIcon(destination)),
-                        contentDescription = destination.route
-                    )
+                    Box{
+                        Icon(
+                            painter = painterResource(id = findIcon(destination)),
+                            contentDescription = destination.route
+                        )
+                        if (destination == Destinations.Favorites && favoriteCount > 0){
+                            Box (
+                                modifier = Modifier
+                                    .size(16.dp)
+                                    .offset(x = 12.dp, y = (-4).dp),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                Icon (
+                                    painter = painterResource(id = R.drawable.red_dot),
+                                    contentDescription = null,
+                                )
+                                Text(
+                                    text = favoriteCount.toString(),
+                                    fontSize = 8.sp,
+                                    color = White
+                                )
+                            }
+                        }
+                    }
+
                 },
                 label = {
                     Text(
