da8a9d68cffd2b9af96019408622435be85fc36c
jakobakoba <borisgabyshev96dev@gmail.com>
2025-06-01 19:01:32 +0900

    ui

--- presentation/src/main/java/com/bor96dev/presentation/composables/SearchHeaderExpanded.kt
+++ presentation/src/main/java/com/bor96dev/presentation/composables/SearchHeaderExpanded.kt
@@ -1,23 +1,35 @@
 package com.bor96dev.presentation.composables
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.bor96dev.presentation.R
+import com.bor96dev.presentation.ui.theme.Blue
+import com.bor96dev.presentation.ui.theme.Grey2
 
 @Composable
 fun SearchHeaderExpanded(
@@ -27,31 +39,50 @@ fun SearchHeaderExpanded(
     Column {
         Row(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically
+                .padding(16.dp)
+                .height(IntrinsicSize.Min)
         ) {
-            IconButton(onClick = onBackClick) {
-                Icon(
-                    painter = painterResource(id = R.drawable.back_ic),
-                    contentDescription = "Назад"
-                )
-            }
-
-            OutlinedTextField(
+            TextField(
                 value = "",
                 onValueChange = {},
-                placeholder = { Text("Должность по подходящим вакан...") }
+                placeholder = {
+                    Text(
+                        "Должность по подходящим вакан...",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                    )
+                },
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.back_ic),
+                        contentDescription = "Назад"
+                    )
+                },
+                colors = TextFieldDefaults.colors(
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent
+                ),
+                shape = RoundedCornerShape(16.dp),
+                modifier = Modifier.weight(1f)
             )
-
-            IconButton(onClick = {}) {
+            Spacer(modifier = Modifier.width(8.dp))
+            IconButton(
+                onClick = {},
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .background(Grey2, shape = RoundedCornerShape(16.dp))
+            ) {
                 Icon(
-                    painter = painterResource(id = R.drawable.filter),
+                    painterResource(id = R.drawable.filter),
                     contentDescription = "Фильтр"
                 )
             }
         }
-        Row(modifier = Modifier.fillMaxWidth()) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,) {
             Text(
                 text = "${vacanciesCount} ${getVacancyWord(vacanciesCount)}",
                 modifier = Modifier.padding(horizontal = 16.dp),
@@ -59,9 +90,13 @@ fun SearchHeaderExpanded(
             )
             Spacer(modifier = Modifier.width(16.dp))
 
-            OutlinedButton(
+            Button(
                 onClick = {},
-                modifier = Modifier.padding(horizontal = 16.dp)
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent,
+                    contentColor = Blue
+                ),
+                elevation = null
             ) {
                 Text("По соответствию")
                 Icon(
