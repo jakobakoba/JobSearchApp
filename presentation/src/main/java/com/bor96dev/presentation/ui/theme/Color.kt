63842c74162e52942cd915084be91c080a246cc8
jakobakoba <borisgabyshev96dev@gmail.com>
2025-06-01 16:32:06 +0900

    add colors

--- presentation/src/main/java/com/bor96dev/presentation/ui/theme/Color.kt
+++ presentation/src/main/java/com/bor96dev/presentation/ui/theme/Color.kt
@@ -9,3 +9,15 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Blue = Color(0xFF2B7EFE)
+val DarkBlue = Color(0xFF00427D)
+val Green = Color(0xFF4CB24E)
+val DarkGreen = Color(0xFF015905)
+val Red = Color(0xFFFF0000)
+
+val Black = Color(0xFF000000)
+val Grey1 = Color(0xFF222325)
+val Grey2 = Color(0xFF313234)
+
+
